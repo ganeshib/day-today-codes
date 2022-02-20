53cf045d090cd65a21f7d36ad20ce681ba17b8ac
ganeshib <60065516+ganeshib@users.noreply.github.com>
2022-02-20 20:41:30 +0530

    Create D13. set-bits.cpp

---
+++ D13. set-bits.cpp
@@ -0,0 +1,16 @@
+https://practice.geeksforgeeks.org/problems/set-bits0143/1
+
+class Solution 
+{
+  public:
+    int setBits(int n) {
+        // Write Your Code here
+        int count=0;
+        while(n!=0)
+        {
+            count++;
+            n=n&n-1;
+        }
+        return count;
+    }
+};
