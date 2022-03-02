39dee9baafa720fb283dbd1f26224c39a247d2ac
ganeshib <60065516+ganeshib@users.noreply.github.com>
2022-03-02 22:48:50 +0530

    Create minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp

---
+++ minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram-ii/
+
+class Solution {
+public:
+    int minSteps(string s, string t) 
+    {
+        int freq[26]={0},ans=0;
+        for(auto it:s) freq[it-'a']++;
+        for(auto it:t) freq[it-'a']--;
+        
+        for(int i=0;i<26;i++)
+        {
+            ans+=abs(freq[i]);
+        }
+        
+        return ans;
+    }
+};
