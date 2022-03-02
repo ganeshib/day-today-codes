b699eaf53060cee05d1a4d6b7475949fe97e19ae
ganeshib <60065516+ganeshib@users.noreply.github.com>
2022-03-02 22:56:30 +0530

    Create D29.excel-sheet-column-number.cpp

---
+++ D29.excel-sheet-column-number.cpp
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/excel-sheet-column-number/
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int ans =0;
+        for(int i=0; i<columnTitle.size() ; i++){
+            ans=ans*26+(columnTitle[i]-'A'+1);
+        }
+        return ans;
+    }
+};
