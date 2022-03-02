1f70344bf8b7da3e7b8d77287f119d7aa4933821
ganeshib <60065516+ganeshib@users.noreply.github.com>
2022-03-02 22:47:59 +0530

    Create D24.minimum-time-to-complete-trips.cpp

---
+++ D24.minimum-time-to-complete-trips.cpp
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/minimum-time-to-complete-trips/
+
+class Solution {
+public:
+    long long minimumTime(vector<int>& time, int totalTrips) 
+    {
+        long long low=0,high=1e14,ans=0;
+        while (low<=high)
+        {
+            long long mid=((high-low)>>1)+low;
+            long long timer=0;
+            for (auto it : time)
+            {
+                timer+=mid/it;
+            }
+            
+         if(timer>=totalTrips){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+            timer=0;
+        }
+            return ans;
+    }
+};
