5f1f246d4364a617ec9b276a39bd6410efc6b7f8
ganeshib <60065516+ganeshib@users.noreply.github.com>
2022-03-10 20:30:09 +0530

    Create D41.kadens_algo.cpp

---
+++ D41.kadens_algo.cpp
@@ -0,0 +1,14 @@
+int maxSubArray(vector<int>& nums) {
+        int maxi=INT_MIN,cursum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(cursum<0)
+            {
+                cursum=0;
+            }
+            cursum+=nums[i];
+            maxi=max(cursum,maxi);
+        }
+        return maxi;
+    }
